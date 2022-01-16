9fda55d7e38fd5df38f0c2e22187c8a43b0bb07d
bayogluteoman <bayogluteoman@gmail.com>
2022-01-16 23:07:18 +0300

    add new design

---
+++ FinalApplication/FinalApplication/FinalApplication/ProfilePage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FinalApplication
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ProfilePage : ContentPage
+    {
+        public ProfilePage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            await EditProfile.Navigation.PushAsync(new UpdateProfilePage());
+        }
+    }
+}
---
+++ FinalApplication/FinalApplication/FinalApplication/UpdateProfilePage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FinalApplication
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class UpdateProfilePage : ContentPage
+    {
+        public UpdateProfilePage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("Successfull", "Profile is up to date!", "OK");
+        }
+    }
+}
